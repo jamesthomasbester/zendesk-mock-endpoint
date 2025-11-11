6f185488a18cb58a79994c34c1c173180c90fe43
James Bester <46641259+jamesthomasbester@users.noreply.github.com>
2025-11-12 10:13:52 +1100

    adding mock auth

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import data from "../data/pharmacy.json";
 import status from "../data/status.json";
+import fullOrders from "../data/full_orders.json";
 
 type RequestBody = {
   orderNumber: string;
@@ -20,29 +21,38 @@ const router = Router();
 
 router.get("/order/tracking/:id", async (req, res) => {
   try {
-    const id = req.params.id;
-    if (id) {
-      const pharmacy =
-        data.Pharmacy[Math.floor(Math.random() * data.Pharmacy.length)];
-      res.status(200).json({
-        name: pharmacy.name,
-        address: pharmacy.address,
-        city: pharmacy.city,
-        state: pharmacy.state,
-        zip_code: pharmacy.zip_code,
-        phone: pharmacy.phone,
-        orderNumber: id,
-        trackingUrl: pharmacy.tracking_url
-      });
-    } else {
-      res.status(400).json({
-        message: `
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token && token === "MockToken") {
+      const id = req.params.id;
+      if (id.startsWith("MPRS")) {
+        const order = fullOrders.orders.find((item) => item.order_code === id);
+        if (order) {
+          res.status(200).json({
+            name: order.pharmacy.name,
+            address: order.pharmacy.address,
+            city: order.pharmacy.city,
+            state: order.pharmacy.state,
+            zip_code: order.pharmacy.zip_code,
+            phone: order.pharmacy.phone,
+            orderNumber: order.order_code,
+            trackingUrl: order.pharmacy.tracking_url
+          });
+        } else {
+          res.status(404).json({ message: "Order not found" });
+        }
+      } else {
+        res.status(400).json({
+          message: `
                 Incorrect Request Body, Please format requests:
                     {
                         "orderNumber": "MPRS**insert order number**"
                     }
                 `
-      });
+        });
+      }
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
     }
   } catch (error) {
     console.error(error);
@@ -52,26 +62,62 @@ router.get("/order/tracking/:id", async (req, res) => {
 
 router.get("/order/status/:id", async (req, res) => {
   try {
-    const id = req.params.id;
-    if (id) {
-      res
-        .status(200)
-        .json({
-          status:
-            status.statuses[
-              Math.floor(Math.random() * status.statuses.length - 1)
-            ]
-        });
-    } else {
-      res.status(400).json({
-        message: `
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token && token === "MockToken") {
+      const id = req.params.id;
+      if (id.startsWith("MPRS")) {
+        const order = fullOrders.orders.find((item) => item.order_code === id);
+        if (order) {
+          res.status(200).json({
+            status: order.status
+          });
+        } else {
+          res.status(404).json({ message: "Order not found" });
+        }
+      } else {
+        res.status(400).json({
+          message: `
                 Incorrect Request Body, Please format requests:
                     {
                         "orderNumber": "MPRS**insert order number**"
                     }
                 `
-      });
+        });
+      }
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/order/tracking/random", async (req, res) => {
+  try {
+    const pharmacy =
+      data.Pharmacy[Math.floor(Math.random() * data.Pharmacy.length)];
+    res.status(200).json({
+      name: pharmacy.name,
+      address: pharmacy.address,
+      city: pharmacy.city,
+      state: pharmacy.state,
+      zip_code: pharmacy.zip_code,
+      phone: pharmacy.phone,
+      orderNumber: pharmacy.id,
+      trackingUrl: pharmacy.tracking_url
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/order/status/random", async (req, res) => {
+  try {
+    res.status(200).json({
+      status:
+        status.statuses[Math.floor(Math.random() * status.statuses.length - 1)]
+    });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
